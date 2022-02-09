2b860e16dc8bed841e6620bcc72eebb98f8b5cdb
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-02-09 16:42:52 +0000

    Update init.php

--- init/init.php
+++ init/init.php
@@ -1,8 +1,8 @@
 <?php
 $configs = getDWConfig();
-if (!defined("TIMEZONE"))
+/*if (!defined("TIMEZONE"))
     define("TIMEZONE", "UTC");
 
-//$logLines = getDWLog();
-
+$logLines = getDWLog();
+*/
 ?>
