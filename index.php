41f3df842151405eda794c6803e8b6ed1d5e863b
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-02-09 15:48:12 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+
+$time = microtime();
+$time = explode(' ', $time);
+$time = $time[1] + $time[0];
+$start = $time;
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+
 echo "test1";
 echo "LOGPATH";
 
