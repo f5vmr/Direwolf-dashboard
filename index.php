c7e90895d321f81f2aa28cfed1b8de564d2132cb
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-02-09 16:38:19 +0000

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,8 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 include "config/config.php";
 include "init/tools.php";
 include "init/functions.php";
-/*include "init/init.php";
-include "version.php";
+include "init/init.php";
+/*include "version.php";
 */
 echo "test1";
 echo $DWLOGPATH;
