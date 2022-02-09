bbc32d92d417897e2924a1031f1823fc4945e74e
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-02-09 14:47:17 +0000

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,16 @@
+//declaration of all variables.
+<?php
+date_default_timezone_set("UK/London");
+define("CONTACTEMAIL", "g4nab.ne63@gmail.com");
+define("LOGPATH", "/var/log");
+define("LOGPREFIX", "dw-start.log");
+define("DWCONFPATH", "/etc/direwolf");
+define("DWCONFFILENAME", "direwolf.conf");
+define("DWLINKPATH", "/usr/bin");
+define("TIMEZONE", "UK/London");
+define("REFRESHAFTER", "10");
+define("SHOWPROGRESSBARS", "on");
+define("SHOWOLDMHEARD", "30");
+define("TEMPERATUREALERT", "on");
+define("TEMPERATUREHIGHLEVEL", "85");
+?>
