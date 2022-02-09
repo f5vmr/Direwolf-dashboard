eb58eef10aba2bbc943bba8d793ed1a2376731ed
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-02-09 16:29:10 +0000

    Update log.php

--- log.php
+++ log.php
@@ -38,7 +38,7 @@ include "init/logs.php";
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary static-top">
     <div class="container">
-      <a class="navbar-brand" href="#">SvxLink Dashboard - <?php echo getConfigItem(MYCALL, $configs); ?></a>
+      <a class="navbar-brand" href="#">SvxLink Dashboard - <?php echo getConfigItem(MYCALL, "MYCALL", $configs); ?></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
