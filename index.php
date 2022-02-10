ba3f5ca5a1e50860b2a9ff6f989d8d2a86af737c
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-02-10 12:29:45 +0000

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,19 @@ include "include/disk.php";
 include "include/sysinfo.php";
 /*include "version.php";
 */
-echo "test1";
+<!DOCTYPE html>
+<html lang="en">
+<!--
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta http-equiv="refresh" content="<?php echo REFRESHAFTER?>">
+  <title>Direwolf Dashboard<?php //echo getConfigItem(MYCALL, "MYCALL", $configs); ?></title>
+  <link rel="stylesheet" href="include/bootstrap.css" 
+</head>
+</html>
 
 ?>
 
