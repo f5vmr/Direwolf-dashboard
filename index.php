2e59286a6ab5fd823c132abeee9578d511e9c393
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-02-10 13:17:26 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,42 @@
 <?php
-/*
 $time = microtime();
 $time = explode(' ', $time);
 $time = $time[1] + $time[0];
 $start = $time;
 header("Cache-Control: no-cache, must-revalidate");
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
-*/
 include "config/config.php";
 include "include/tools.php";
 include "include/functions.php";
 include "include/init.php";
 //include "include/disk.php";
 //include "include/sysinfo.php";
-/*include "version.php";
-*/
+include "version.php";
+
 ?>
+
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
+
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
   <meta http-equiv="refresh" content="<?php echo REFRESHAFTER?>">
+  
   <title>Direwolf Dashboard</title>
+  
   <link rel="stylesheet" href="include/style.css" 
-</head>
+
+ </head>
+ 
+ <!-- Navigation -->
+  
  <nav class="navbar navbar-expand-lg navbar-dark bg-primary static-top">
     <div class="container">
-      <a class="navbar-brand" href="#">Direwolf Dashboard - <?php //echo getConfigItem(SVXLOGICSECTION, "CALLSIGN", $configs); ?></a>
+      <a class="navbar-brand" href="#">Direwolf Dashboard - </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -50,6 +57,17 @@ include "include/init.php";
       </div>
     </div>
   </nav>
+
+<!-- Page content -->
+  
+  <div class="container-fluid">
+    <div class="row">
+      <?php
+        include "include/sysinfo.php";
+        include "include/disk.php";
+      ?>
+    </div>
+  </div>
 
 </html>
 
