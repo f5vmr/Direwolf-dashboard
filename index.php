e482732c818f6567bb732b01ff490676971646cb
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-02-10 12:33:37 +0000

    Update index.php

--- index.php
+++ index.php
@@ -15,9 +15,10 @@ include "include/disk.php";
 include "include/sysinfo.php";
 /*include "version.php";
 */
+?>
 <!DOCTYPE html>
 <html lang="en">
-<!--
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -29,6 +30,6 @@ include "include/sysinfo.php";
 </head>
 </html>
 
-?>
+
 
 
