db460cbe03a8e09370579e7086ae30c422d8022f
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-02-10 14:13:10 +0000

    Update index.php

--- index.php
+++ index.php
@@ -12,11 +12,11 @@
   <title>Direwolf Dashboard</title>
   <link rel="stylesheet" href="include/style.css" 
 </head>
- <?php
+ <?php /*
 $time = microtime();
 $time = explode(' ', $time);
 $time = $time[1] + $time[0];
-$start = $time;
+$start = $time; */
 header("Cache-Control: no-cache, must-revalidate");
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 include "config/config.php";
