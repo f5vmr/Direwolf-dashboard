61d2e79342a5af59bd4073e32421413765e5dbba
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-02-10 12:37:47 +0000

    Update index.php

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ include "config/config.php";
 include "include/tools.php";
 include "include/functions.php";
 include "include/init.php";
-include "include/disk.php";
-include "include/sysinfo.php";
+//include "include/disk.php";
+//include "include/sysinfo.php";
 /*include "version.php";
 */
 ?>
