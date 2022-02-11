39c0b1324dbad8d2d44bc63e5a035db495f142b2
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-02-11 19:02:12 +0000

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ include "include/functions.php";
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary static-top">
     <div class="container">
-      <a class="navbar-brand" href="#">Direwolf  Dashboard for <?php echo getConfigItem(SVXLOGICSECTION, "CALLSIGN", $configs); ?></a>
+      <a class="navbar-brand" href="#">Direwolf  Dashboard for <?php echo getConfigItem(MYCALL, "MYCALL", $configs); ?></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
