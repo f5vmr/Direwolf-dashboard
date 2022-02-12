e968e6a5e5d55159ec742abe932b6bdd73a66fb5
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-02-12 10:13:01 +0000

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
   <meta http-equiv="refresh" content="Refreshing code goes here">
-  <title>Direwolf Dashboard for <?php echo getDWConfig(); echo "G4NAB"; ?> </title>
+  <title>Direwolf Dashboard for <?php echo getConfigItem("MYCALL", $config); echo "G4NAB"; ?> </title>
   <link rel="stylesheet" href="include/style.css" 
 </head>
  <?php 
