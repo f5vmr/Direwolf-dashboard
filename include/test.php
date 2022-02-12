1f8b8254d242cea26d1be41388c77fe75bfd0805
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-02-12 13:07:02 +0000

    Update test.php

--- include/test.php
+++ include/test.php
@@ -1,7 +1,7 @@
 <?php
-include "include/functions.php"
-include "include/tools.php"
-define("FILENAME","/etc/direwolf/direwolf.conf")
+include "include/functions.php";
+include "include/tools.php";
+define("FILENAME","/etc/direwolf/direwolf.conf");
 getDWConfig();
 print "success";
 ?>
