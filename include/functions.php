64802531a0655a36d2fec174c0268fe5308d9cb7
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-02-12 13:40:47 +0000

    Update functions.php

--- include/functions.php
+++ include/functions.php
@@ -6,7 +6,7 @@ function getDWConfig() {
 	
         $configs = fopen(DWCONFPATH.DWCONFFILENAME, 'r')) {
                 while ($config = fgets($configs)) {
-                        array_push($conf, trim( $config, " \t\n\r\0\x0B"): string);
+                        array_push($conf, trim( $config, " \t\n\r\0\x0B"): $config);
                 }
 		fclose($configs);
 	}
