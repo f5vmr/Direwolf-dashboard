16d112a906f6180cc6e892831147b4b8532a5315
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-02-12 09:57:07 +0000

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
   <meta http-equiv="refresh" content="Refreshing code goes here">
-  <title>Direwolf Dashboard for <?php echo getConfigItem("MYCALL", $configs); ?> </title>
+  <title>Direwolf Dashboard for <?php $array() = DWConfig(DWCONFPATH."/".DWCONFFILENAME); echo getConfigItem("MYCALL", $configs); ?> </title>
   <link rel="stylesheet" href="include/style.css" 
 </head>
  <?php 
