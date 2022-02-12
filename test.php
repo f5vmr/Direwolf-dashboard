9f6d8de69ebcbb534ab9ffdc3a803e7ebbd4b218
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-02-12 15:59:57 +0000

    Create test.php

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+include "include/functions.php";
+include "include/tools.php";
+$array_length = count($confLines);
+$i=0;
+while ($i < $array_length) {
+ echo confLines[$i] . "<br />";
+  $i++
+  }
+?>
+
+   
