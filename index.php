4dc956a0024fad29ae55934958e81b2ef81e4953
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-02-12 17:19:14 +0000

    Update index.php

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ include "version.php";
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary static-top">
     <div class="container">
-      <a class="navbar-brand" href="#">Direwolf  Dashboard for <?php //echo getConfigItem(MYCALL,$confLines);?></a>
+	    <a class="navbar-brand" href="#"><h1>Direwolf  Dashboard</h1><?php //echo getConfigItem(MYCALL,$confLines);?></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
