1e0d0cf11bc56379c3baac6234f30976f40b44c3
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-02-12 16:11:43 +0000

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,12 +1,12 @@
 <?php
 date_default_timezone_set('UK/London');
 define("CONTACTEMAIL", "g4nab.ne63@gmail.com");
-define("DWLOGPATH", "/var/log/");
+define("DWLOGPATH", "/var/log");
 define("DWLOGPREFIX", "daemon.log");
-define("DWCONFPATH", "/etc/direwolf/");
+define("DWCONFPATH", "/etc/direwolf");
 define("DWCONFFILENAME", "direwolf.conf");
 define("MYCALL", "MYCALL");
-define("DWLINKPATH", "/usr/bin/");
+define("DWLINKPATH", "/usr/bin");
 define("TIMEZONE", "UK/London");
 define("REFRESHAFTER", "10");
 define("SHOWPROGRESSBARS", "on");
